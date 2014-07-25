32dc80586313dd3907d1a19ebcda9c6927de958d
Klos71 <Klos71@192.168.1.133>
2014-07-26 00:31:57 +0300

    added so items drop when game starts DROPPARTY!!!!!!!!!

--- Ba/src/ba/idrol/Game/Game.java
+++ Ba/src/ba/idrol/Game/Game.java
@@ -9,24 +9,33 @@ public class Game extends GameComponent {
 	GameObject bg;
 	GameObject plr;
 	GameObject ground, platform_1, platform_2, platform_3, platform_4, platform_5, platform_6, platform_7, platform_8;
+	GameObject sw_1, sw_2, sh_1, sh_2;
 	public Game(){
 		
 	}
 	@Override
 	public void loadObjects(){
 		bg = new GameObject(800, 600, new Sprite("/res/images/world/bg.png"), 0, 0).disableCollision();
-		plr = new Player(new Sprite("/res/images/character/char.png"), 30, 50).enableGravity();
-		System.out.println(plr.getCollStat());
+		
 		ground = new GameObject(800, 10, new Sprite("/res/images/world/sand.png"), 0, 0);
 		Sprite platform = new Sprite("/res/images/world/platform.png");
+		Sprite sword = new Sprite("/res/images/items/sword.png");
+		Sprite shield = new Sprite("/res/images/items/shield.png");
 		platform_1 = new GameObject(platform, 100, 60);
 		platform_2 = new GameObject(platform, 400, 100);
 		platform_3 = new GameObject(platform, 600, 60);
-		platform_4 = new GameObject(platform, 500, 160);
+		platform_4 = new GameObject(platform, 500, 160); 
 		platform_5 = new GameObject(platform, 310, 260);
 		platform_6 = new GameObject(platform, 120, 360);
 		platform_7 = new GameObject(platform, 290, 460);
 		platform_8 = new GameObject(platform, 400, 560);
+		sw_1 = new GameObject(sword, 310, 700).enableGravity().disableCollision();
+		sw_2 = new GameObject(sword, 410, 700).enableGravity().disableCollision();
+		sh_1 = new GameObject(shield, 600, 700).enableGravity().disableCollision();
+		sh_2 = new GameObject(shield, 200, 700).enableGravity().disableCollision();
+		
+		plr = new Player(new Sprite("/res/images/character/char.png"), 30, 50).enableGravity();
+		System.out.println(plr.getCollStat());
 	}
 	@Override
 	public void update(int delta){
